28ec3c05d19bab306c18989096ac35b5af8d9ba6
zhzh12312312 <111668120+zhzh12312312@users.noreply.github.com>
2023-04-23 16:02:03 +0800

    4

---
+++ hms_frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <ManagerView/>
+</template>
+
+<script>
+import ManagerView from './components/ManagerView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ManagerView
+  }
+}
+</script>
